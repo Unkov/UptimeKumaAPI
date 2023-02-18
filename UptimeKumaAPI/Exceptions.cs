677b2393368af06079f41d86bb321b4f07a01d7d
kolya112 <nikolayurch@gmail.com>
2023-02-18 21:30:24 +0300

    Added string GetStatusPageConfig(string statusPage) method for getting status page config

---
+++ UptimeKumaAPI/Exceptions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace UptimeKumaAPI
+{
+    public class APIConnectException : Exception
+    {
+        public APIConnectException() : base() { }
+    }
+}
--- UptimeKumaAPI/KumaAPIHub.cs
+++ UptimeKumaAPI/KumaAPIHub.cs
@@ -1,9 +1,39 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace UptimeKumaAPI
 {
     public class KumaAPIHub
     {
+        protected static string _baseURL = null;
 
+        /// <summary>
+        /// Main constructor of class
+        /// </summary>
+        /// <param name="url">URL of site with Uptime Kuma</param>
+        public KumaAPIHub(string url)
+        {
+            _baseURL = url;
+        }
+
+        /// <summary>
+        /// Get status page config
+        /// </summary>
+        /// <param name="statusPage">Name of status page. Default is "default"</param>
+        /// <returns></returns>
+        public string GetStatusPageConfig(string statusPage = "default")
+        {
+            var result = Request($"/api/status-page/{statusPage}");
+            string answer = new StreamReader(result.GetResponseStream()).ReadToEnd();
+            return JObject.Parse(answer).SelectToken("config").ToString();
+        }
+
+        protected static HttpWebResponse Request(string suburl, string parametrs = "")
+        {
+            return (HttpWebResponse)WebRequest.Create($"{_baseURL}{suburl}?{parametrs}").GetResponse();
+        }
     }
 }
